81e8baced68abc7ecdf644feb2a9a6a8239b08ab
richarrrdv <107338752+richarrrdv@users.noreply.github.com>
2022-10-29 04:01:38 -0700

    Add files via upload

--- src/Components/Block.js
+++ src/Components/Block.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 const Block = (props) => {
     /* TODO */
+    let colorposts = {backgroundColor: props.color }
 
     return (
         // ignore this data-testid, it's purely for testing :)
@@ -11,8 +12,8 @@ const Block = (props) => {
             we took care of the basic block styling! 
             If curious, check out the css in Styles/Block.css */}
             {/* Refer to hint if stuck on how to color the posts */}
-            <div className="post"></div>
-            <div className="caption"></div>
+            <div className="post" style = {colorposts}></div>
+            <div className="caption">{props.color}</div>
         </div>
     );
 }
--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,11 +1,15 @@
 import React from 'react';
 /* Add any imports you think you might need here! */
+import Color from './Color';
 
-const Menu = () => { 
+const Menu = (props) => { 
 
     return (
       <div className="colorOptions">
-          {/* TODO */}
+          <Color color="red" handleClick={props.addblocks}/>
+          <Color color="pink" handleClick={props.addblocks}/>
+          <Color color="blue" handleClick={props.addblocks}/>
+          <Color color="green" handleClick={props.addblocks}/>
       </div>
     );
 }
