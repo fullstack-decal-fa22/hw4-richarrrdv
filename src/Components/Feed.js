df4e5c1b31e13519c6ea341993d6f042632885c6
richarrrdv <107338752+richarrrdv@users.noreply.github.com>
2022-10-29 04:35:38 -0700

    commit

--- src/Components/Feed.js
+++ src/Components/Feed.js
@@ -1,18 +1,19 @@
-import { useState } from 'react';
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from './Menu';
 import Block from './Block';
 
 const Feed = () => {
     /* TODO: Declare a new state variable to keep track of the blocks on your Blockstagram feed! */
     // Refer to Hint 2 for more help!
+    const [blocks, setblocks] = useState([]);
 
     /* Use the map() function to render multiple Blocks! */
-    const posts = null; // TODO: edit this variable
+    const posts = blocks.map((blocks) => <Block color={blocks}/>); // TODO: edit this variable
 
+    const addblocks = (color) => {setblocks([color, ...blocks])}
     return (
         <div>
-            <Menu></Menu>
+            <Menu addblocks={addblocks}></Menu>
 
             {/* Below is where all of your Blocks should render! */}
             {posts}
